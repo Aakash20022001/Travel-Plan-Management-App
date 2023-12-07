d5aafed174865f216d5154d0d454f0fa88f6fc89
Aakash20022001 <70940981+Aakash20022001@users.noreply.github.com>
2023-12-07 14:29:10 +0530

    Update TravelPlanService.java

--- Backend/src/main/java/com/cspl/travelplanmanagement2/Services/TravelPlanService.java
+++ Backend/src/main/java/com/cspl/travelplanmanagement2/Services/TravelPlanService.java
@@ -1,19 +1,20 @@
 package com.cspl.travelplanmanagement2.Services;
 
 import java.util.List;
-
 import com.cspl.travelplanmanagement2.Dto.TravelPlanDTO;
+import com.cspl.travelplanmanagement2.Dto.UserDTO;
 
 public interface TravelPlanService {
 
-	List<TravelPlanDTO> getAllTravelPlans();
+    List<TravelPlanDTO> getAllTravelPlans();
 
-	TravelPlanDTO getTravelPlanById(Long id);
+    TravelPlanDTO getTravelPlanById(Long id);
 
-	Long createTravelPlan(TravelPlanDTO travelPlanDTO);
+    Long createTravelPlan(TravelPlanDTO travelPlanDTO);
 
-	TravelPlanDTO updateTravelPlan(Long id, TravelPlanDTO travelPlanDTO);
+    TravelPlanDTO updateTravelPlan(Long id, TravelPlanDTO travelPlanDTO);
 
-	void deleteTravelPlan(Long id);
+    void deleteTravelPlan(Long id);
 
+    List<UserDTO> getUsersForTravelPlan(Long travelPlanId);
 }
